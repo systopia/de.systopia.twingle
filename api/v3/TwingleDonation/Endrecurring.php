bd6c60c5390a587a09135a4916b4091a033a64ac
B. Endres <endres@systopia.de>
2023-05-15 12:03:32 +0200

    replaced old debug-log call with current one.

--- api/v3/TwingleDonation/Endrecurring.php
+++ api/v3/TwingleDonation/Endrecurring.php
@@ -60,7 +60,7 @@ function _civicrm_api3_twingle_donation_endrecurring_spec(&$params) {
 function civicrm_api3_twingle_donation_endrecurring($params) {
   // Log call if debugging is enabled within civicrm.settings.php.
   if (defined('TWINGLE_API_LOGGING') && TWINGLE_API_LOGGING) {
-    CRM_Core_Error::debug_log_message('TwingleDonation.Endrecurring: ' . json_encode($params, JSON_PRETTY_PRINT));
+    Civi::log()->debug('TwingleDonation.Endrecurring: ' . json_encode($params, JSON_PRETTY_PRINT));
   }
 
   try {
--- api/v3/TwingleDonation/Submit.php
+++ api/v3/TwingleDonation/Submit.php
@@ -265,7 +265,7 @@ function _civicrm_api3_twingle_donation_Submit_spec(&$params) {
 function civicrm_api3_twingle_donation_Submit($params) {
   // Log call if debugging is enabled within civicrm.settings.php.
   if (defined('TWINGLE_API_LOGGING') && TWINGLE_API_LOGGING) {
-    CRM_Core_Error::debug_log_message('TwingleDonation.Submit: ' . json_encode($params, JSON_PRETTY_PRINT));
+    Civi::log()->debug('TwingleDonation.Submit: ' . json_encode($params, JSON_PRETTY_PRINT));
   }
 
   try {
