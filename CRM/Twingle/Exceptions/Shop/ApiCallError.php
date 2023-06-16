00863f45bddf8142d6306fd7d4b9cc33f318f778
Marc Michalsky <michalsky@forumzfd.de>
2023-06-16 13:31:52 +0200

    move BaseExeption.php
    Move the BaseExeption class to anticipate some changes in a branch developed in parallel to improve error handling, which also inherits from BaseExeption.

--- CRM/Twingle/Exceptions/Shop/ApiCallError.php
+++ CRM/Twingle/Exceptions/Shop/ApiCallError.php
@@ -4,7 +4,7 @@
  * A simple custom exception that indicates a problem within the
  * CRM_Twingle_Shop_ApiCall class
  */
-class CRM_Twingle_Exceptions_Shop_ApiCallError extends CRM_Twingle_Exceptions_Shop_BaseException {
+class CRM_Twingle_Exceptions_Shop_ApiCallError extends CRM_Twingle_Exceptions_BaseException {
   public const ERROR_CODE_API_TOKEN_MISSING = "api_token_missing";
   public const ERROR_CODE_CONNECTION_FAILED = "connection_failed";
   public const ERROR_CODE_NOT_CONNECTED = "not_connected";
--- CRM/Twingle/Exceptions/Shop/ShopException.php
+++ CRM/Twingle/Exceptions/Shop/ShopException.php
@@ -4,7 +4,7 @@
  * A simple custom exception that indicates a problem within the
  * CRM_Twingle_Shop class
  */
-class CRM_Twingle_Exceptions_Shop_ShopException extends CRM_Twingle_Exceptions_Shop_BaseException {
+class CRM_Twingle_Exceptions_Shop_ShopException extends CRM_Twingle_Exceptions_BaseException {
 
   public const ERROR_CODE_NOT_A_SHOP = "not_a_shop";
   public const ERROR_CODE_COULD_NOT_GET_PROJECTS = "could_not_get_projects";
