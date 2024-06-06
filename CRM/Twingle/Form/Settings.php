a0b2879b694c4eb7bdb4a3208c45e4ace97e5fad
Jens Schuppe <schuppe@systopia.de>
2024-06-06 10:56:49 +0200

    Prevent undefined index warnings for unchecked checkbpxes in the settings form

--- CRM/Twingle/Form/Settings.php
+++ CRM/Twingle/Form/Settings.php
@@ -157,7 +157,7 @@ class CRM_Twingle_Form_Settings extends CRM_Core_Form {
 
     // store settings
     foreach (self::$SETTINGS_LIST as $setting) {
-      Civi::settings()->set($setting, $values[$setting]);
+      Civi::settings()->set($setting, $values[$setting] ?? NULL);
     }
 
     parent::postProcess();
