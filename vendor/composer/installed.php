c98aa6c8d12275b904fbce101036ebf381dcf170
Jens Schuppe <schuppe@systopia.de>
2026-02-03 15:59:58 +0100

    Set version to 1.7.0-beta1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.twingle',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '031e2860efc0562a95b63abe45fcbcc123647e9e',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.twingle' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '031e2860efc0562a95b63abe45fcbcc123647e9e',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
