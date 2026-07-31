06164282bda3a761b7ffddd68e6c69029358702d
Dominic Tubach <tubach@systopia.de>
2026-07-31 17:58:01 +0200

    Set version to 1.8.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.twingle',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7843bcfdb4c216fa0f4bfeb8f1dae324652a068c',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.8.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.twingle' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7843bcfdb4c216fa0f4bfeb8f1dae324652a068c',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.8.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
